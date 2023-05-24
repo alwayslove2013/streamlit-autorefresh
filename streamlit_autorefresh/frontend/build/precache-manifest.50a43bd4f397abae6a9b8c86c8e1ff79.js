6fa0a3591a29272a747bbbfa60688513945c6876
min.tian <min.tian.cn@gmail.com>
2023-05-24 11:22:12 +0800

    support streamlit-nightly
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

---
+++ streamlit_autorefresh/frontend/build/precache-manifest.50a43bd4f397abae6a9b8c86c8e1ff79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63708ed1f533652e1b67074f2b57d370",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ede3e44855b7b2e447f",
+    "url": "./static/js/2.0b5b222e.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.0b5b222e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f74f7a079db9cec6d3",
+    "url": "./static/js/main.c162d2b4.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
